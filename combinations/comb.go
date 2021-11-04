2e1a70bc8856f7b9508bc3fae782ad8651dbe2ff
mireia <mireiaolmo@gitlab.com>
2021-11-04 17:52:48 +0100

    meitat de combinacions per a 1 grup d 3

--- combinations/comb.go
+++ combinations/comb.go
@@ -33,11 +33,13 @@ func List(c [WORDS]int) int {
 			wg.Add(1)
 			go getCombs(c, g, cases, &wg)
 		case <-exit:
+			fmt.Println("waiting")
 			wg.Wait()
 			for i := 0; i < len(cases); {
 				num := <-cases
 				total += num
 			}
+			fmt.Println("done")
 			return total
 		}
 	}
@@ -53,8 +55,11 @@ func GetGroups(c [WORDS]int, groups chan [GROUP]int, exit chan bool) {
 				slice[2] = c[k]
 				fmt.Println("...Sending slice...", slice)
 				groups <- slice
+				break
 			}
+			break
 		}
+		break
 	}
 	exit <- true
 }
@@ -76,9 +81,10 @@ func checkIn(slice [GROUP]int, combs [][GROUP]int) bool {
 	return false
 }
 
-func search(c [WORDS]int, g [GROUP]int, slices chan [GROUP]int, combs [][GROUP]int) {
+func search(c [WORDS]int, g [GROUP]int, slices chan [GROUP]int, stop chan bool) {
+	var combs [][GROUP]int
 	var colInOriginal bool
-	var done bool
+	var top bool
 	limit := GROUP - 1
 	slice := [GROUP]int{}
 
@@ -86,12 +92,30 @@ func search(c [WORDS]int, g [GROUP]int, slices chan [GROUP]int, combs [][GROUP]i
 	//	{4,6,7} -> {1,2,3},{1,2,5},{1,2,8},{1,3,5},{1,3,8},{1,5,8},{2,3,5},{2,3,8},{2,5,8},{3,5,8}
 	//	{2,3,7} -> {1,4,5},{1,4,6},{1,4,8},{1,5,6},{1,5,8},{1,6,8},{4,5,6},{4,5,8},{4,6,8},{5,6,8}
 
+	//	for {
 	k := 0
-	for i := 0; i < GROUP; i++ {
+	for i := 0; i < GROUP; {
+		fmt.Println("slice when done", slice)
+		if top {
+			fmt.Println("limit", limit)
+			//limit = 2
+			//i=2
+			for o := GROUP; o > limit; o-- {
+				i--
+				k = slice[i] + 1
+				slice[i] = 0
+			}
+			top = false
+			fmt.Println("done", top)
+		}
+		fmt.Println("i: ", i)
 		for j := k; j < WORDS; j++ {
+			fmt.Println("k: ", k)
 			colInOriginal = false
 			if slice[i] != 0 {
-				break //ó j=WORDS
+				fmt.Println("slice[i]!=0")
+				i++
+				break
 			} else {
 				//comprobamos que no este ya la columna en el mismo grupo. Por ejemplo: {2,3,7}
 				for _, v := range g {
@@ -100,57 +124,72 @@ func search(c [WORDS]int, g [GROUP]int, slices chan [GROUP]int, combs [][GROUP]i
 						break
 					}
 				}
+				fmt.Println("colInOriginal", colInOriginal)
 				if !colInOriginal {
 					switch i {
 					case GROUP - 1:
-						slice[i] = c[j]
-						if !checkIn(slice, combs) {
+						fmt.Println("case i==GROUP-1")
+						if c[j] > slice[i-1] {
+							fmt.Println("c[j] > slice[i-1]")
 							slice[i] = c[j]
-							slices <- slice
-						} else {
+							if !checkIn(slice, combs) {
+								fmt.Println("!checkIn")
+								slice[i] = c[j]
+								combs = append(combs, slice)
+								slices <- slice
+							} else {
+								fmt.Println("checkIn. slice[i]=0")
+								slice[i] = 0
+							}
+							if j == WORDS-1 {
+								top = true
+								limit = i
+								fmt.Println("done", top)
+							}
 							slice[i] = 0
-						}
-						if k == WORDS-1 {
-							done = true
 						}
 					//comprobamos q no este ya contado el caso
 					case GROUP - 2:
-						slice[i] = c[j]
-						if k == WORDS-1 {
-							limit = i
+						fmt.Println("case 2==GROUP-2")
+						if c[j] > slice[i-1] {
+							fmt.Println("c[j] > slice[i-1]")
+							slice[i] = c[j]
+							if j == WORDS-1 {
+								limit = i
+								fmt.Println("limit", limit)
+							}
 						}
 						//					case GROUP - 3:
 					default:
+						fmt.Println("default case")
 						slice[i] = c[j]
+						if j == WORDS-1 {
+							limit = i
+							fmt.Println("limit", limit)
+						}
 					}
 				}
 			}
-		}
-		if done {
-			done = false
-			for o := GROUP; o > limit; o-- {
-				k = slice[i] + 1
-				slice[i] = 0
-				i--
+			if i == 0 && j == WORDS-1 {
+				stop <- true
 			}
-			i--
 		}
 	}
 }
 
 func getCombs(c [WORDS]int, g [GROUP]int, cases chan int, wg *sync.WaitGroup) {
-	var combs [][GROUP]int
 	slices := make(chan [GROUP]int)
 	stop := make(chan bool)
+	var total int
 
-	go search(c, g, slices, combs)
+	go search(c, g, slices, stop)
 	for {
 		select {
-		case comb := <-slices:
-			combs = append(combs, comb)
-			cases <- 1
-			//			go search(c, g, slices, combs)
+		case s := <-slices:
+			total++
+			fmt.Println("slice", total, s[:])
 		case <-stop:
+			cases <- total
 			wg.Done()
 			return
 		}
